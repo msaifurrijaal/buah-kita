5f6559127c8e94a588c58c3c9fcd710ad8ac402f
Muchammad Saifurrijaal <msaifurrijaal@gmail.com>
2024-01-31 11:08:23 +0700

    fix(invoice): fixing call component invoice layout

--- src/pages/invoice/index.tsx
+++ src/pages/invoice/index.tsx
@@ -2,6 +2,7 @@ import { useLocation } from "react-router-dom";
 import MainLayout from "../../components/partials/layout/MainLayout";
 import { Invoice } from "../../types/interfaces/invoice";
 import { useEffect } from "react";
+import InvoiceLayout from "../../components/fragments/invoice/InvoiceLayout";
 
 const InvoicePage = () => {
   const location = useLocation();
@@ -17,7 +18,7 @@ const InvoicePage = () => {
     <MainLayout>
       <div className="container py-16 md:py-0">
         <div className="w-full p-6 min-h-screen flex justify-center items-center">
-          
+          <InvoiceLayout invoice={invoice}/>
         </div>
       </div>
     </MainLayout>
